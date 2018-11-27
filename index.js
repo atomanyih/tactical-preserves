bbf57f0f875555032b273b708613c1b5bb69bf8d
August Toman-Yih <august.toman.yih@gmail.com>
2018-11-26 20:35:04 -0800

    animate some donk

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const CENTER_X = 430;
 const CENTER_Y = 200;
 const OUTER_RADIUS = 200;
-const NUM_LAYERS = 5;
-const INNER_RADIUS = 50;
+// const NUM_LAYERS = 5;
+const NUM_LAYERS = 20;
+const INNER_RADIUS = 100;
 const ANGLE_OFFSET = 5;
 const layerWidth = (OUTER_RADIUS - INNER_RADIUS) / NUM_LAYERS
 
@@ -11,23 +12,38 @@ const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
 const image = new Image();
-image.addEventListener('load', () => {
-  canvas.width = image.width;
-  canvas.height = image.height;
 
+function animate(fn) {
+  function loop(t) {
+    fn(t);
+    requestAnimationFrame(loop)
+  }
+
+  requestAnimationFrame(loop);
+}
+
+function render(t) {
   ctx.drawImage(image, 0, 0);
 
-  for(let i = 0; i < NUM_LAYERS; i++) {
+  let angleOffset = (1 * t / 1000) % 360;
+  // let angleOffset = 5;
+
+  for (let i = 0; i < NUM_LAYERS; i++) {
+    ctx.save();
     ctx.beginPath();
     ctx.arc(CENTER_X, CENTER_Y, OUTER_RADIUS - i * layerWidth, 0, Math.PI * 2, true);
     ctx.clip();
     ctx.translate(CENTER_X, CENTER_Y);
-    ctx.rotate((i+1) * ANGLE_OFFSET * Math.PI / 180);
+    ctx.rotate((i + 1) * angleOffset * Math.PI / 180);
     ctx.translate(-CENTER_X, -CENTER_Y);
     ctx.drawImage(image, 0, 0);
     ctx.restore();
   }
+}
 
-
+image.addEventListener('load', () => {
+  canvas.width = image.width;
+  canvas.height = image.height;
+  animate(render);
 }, false)
 image.src = 'abandoned-acoustic-mirrors-denge.jpg'
